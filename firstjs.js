c547805ba1d91536c123afe655bfd18e3e9182a3
Edward <edward.b.workin@gmail.com>
2022-10-19 19:07:15 -0400

    Stage 2 of animation (next step responsiveness)

--- firstjs.js
+++ firstjs.js
@@ -2,23 +2,48 @@ const pickerRed = document.querySelector('.red')
 const pickerBlue = document.querySelector('.blue')
 const pickerGreen = document.querySelector('.green')
 const pickerPurple = document.querySelector('.purple')
+const dotRed = document.querySelector('#dot-r')
+const dotBlue = document.querySelector('#dot-b')
+const dotGreen = document.querySelector('#dot-g')
+const dotPurple = document.querySelector('#dot-p')
 const subject = document.querySelector('h1')
 
 const cls = ['cred', 'cblue', 'cgreen', 'cpurple']
 
 pickerRed.addEventListener("click", e =>{
-    subject.classList.remove(...cls)
-    subject.classList.add("cred")
+    dotRed.classList.add("active")
+    function colorChange() {
+        subject.classList.remove(...cls);
+        subject.classList.add("cred");
+        dotRed.classList.remove("active");
+    }
+    setTimeout(colorChange,500)
+    
 })
 pickerBlue.addEventListener("click", e =>{
-    subject.classList.remove(...cls)
-    subject.classList.add("cblue")
+    dotBlue.classList.add("active")
+    function colorChange() {
+        subject.classList.remove(...cls);
+        subject.classList.add("cblue");
+        dotBlue.classList.remove("active");
+    }
+    setTimeout(colorChange,500)
 })
 pickerGreen.addEventListener("click", e =>{
-    subject.classList.remove(...cls)
-    subject.classList.add("cgreen")
+    dotGreen.classList.add("active")
+    function colorChange() {
+        subject.classList.remove(...cls);
+        subject.classList.add("cgreen");
+        dotGreen.classList.remove("active");
+    }
+    setTimeout(colorChange,500)
 })
 pickerPurple.addEventListener("click", e =>{
-    subject.classList.remove(...cls)
-    subject.classList.add("cpurple")
+    dotPurple.classList.add("active")
+    function colorChange() {
+        subject.classList.remove(...cls);
+        subject.classList.add("cpurple");
+        dotPurple.classList.remove("active");
+    }
+    setTimeout(colorChange,500)
 })
